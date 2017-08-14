bb65ae7d2df54927f5ce1b55680f44206d3f545f
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-14 21:20:00 +0530

    Added head.php to make sure that all files under use have common heading html

---
+++ head.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Campaign Treasurer Companion</title>
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="base.css" type="text/css" />
+        <meta name="theme-color" content="#326e82">
+        <meta name="author" content="SwG Ghosh">
+        <meta name="description" content="The Campaign Treasurer Companion Web App for Prayas'17 fest.">
+        <meta name="og:url" content="http://campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com">
+    </head>
+    <body>
+        <div class="bars">
+            <div class="topbar">
+                <h1>
+                    Campaign Treasurer Companion
+                </h1>
+                <small class="time">18/08/17 20:00:00</small>
+            </div>
+            <div class="bottombar">
+                <ul class="tabs">
+                    <a href="index.php" class="tabitem"><li id="selected"><span class="emoji">🏠</span><br>Home</li></a>
+                    <a href="details.php" class="tabitem"><li><span class="emoji">📈</span><br>Details</li></a>
+                    <a href="trader.php" class="tabitem"><li><span class="emoji">💵</span><br>Trader</li></a>
+                    <a href="news.php" class="tabitem"><li><span class="emoji">📰</span><br>News</li></a>
+                    <a href="about.php" class="tabitem"><li><span class="emoji">🔬</span><br>About</li></a>
+                </ul>
+            </div>
+        </div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include('head.php'); ?>
+        <table class="view">
+            <tr class="sector"><td colspan="4" class="sector">Crypto-currency</td></tr>
+            <tr class="stock"><td class="name">Bitcoin</td><td class="current" colspan="3">$10.0</td></tr>
+            <tr class="stock"><td class="name">Ripple</td><td class="current" colspan="3">$10.0</td></tr>
+            <tr class="stock"><td class="name">Ether</td><td class="current" colspan="3">$10.0</td></tr>
+            <tr class="sector"><td colspan="4" class="sector">Commodity</td></tr>
+            <tr class="stock"><td class="name">Gold</td><td class="current" colspan="3">$10.0</td></tr>
+            <tr class="stock"><td class="name">Silver</td><td class="current" colspan="3">$10.0</td></tr>
+            <tr class="stock"><td class="name">Crude Oil</td><td class="current" colspan="3">$10.0</td></tr>
+            <tr class="sector"><td colspan="4" class="sector">Securities</td></tr>
+            <tr class="stock"><td class="name">Apple<br><small>Technology</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">General Motors<br><small>Automotive</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+            <tr class="stock"><td class="name">Microsoft<br><small>Technology</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">Tesla<br><small>Automotive</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+            <tr class="stock"><td class="name">Amazon<br><small>E-Commerce</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">Netflix<br><small>Entertainment</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+            <tr class="stock"><td class="name">Facebook<br><small>Technology</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">Starbucks<br><small>Fast Moving Consumer Goods</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+            <tr class="stock"><td class="name">Berkshire Hathway<br><small>Diversified</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">Johnson and Johnson<br><small>Fast Moving Consumer Goods</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+            <tr class="stock"><td class="name">JP Morgan<br><small>Finance</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">General Electric<br><small>Electric</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+            <tr class="stock"><td class="name">Walmart<br><small>Retail</small></td><td class="current">$10.0</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$2.0</td><td class="percentage high">1.30%</td></tr>
+            <tr class="stock"><td class="name">Alibaba<br><small>E-Commerce</small></td><td class="current">$10.0</td><td class="difference"><span class="inverted triangle">&#9660;</span>$2.0</td><td class="percentage low">1.30%</td></tr>
+        </table>
+    </body>
+</html>
