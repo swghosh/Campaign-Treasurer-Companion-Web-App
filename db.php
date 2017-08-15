221aad081e91775746b2b940992fa0ac0e019c38
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-15 19:38:19 +0530

    Updated db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 //Set database settings
-$host = getenv("RDS_HOST_NAME");
+$host = getenv("RDS_HOSTNAME");
 $username = getenv("RDS_USERNAME");
 $password = getenv("RDS_PASSWORD");
 $port = getenv("RDS_PORT");
-$database = getenv("RDS_DB_NAME");
+$database = getenv("RDS_DB");
 //-----------------------------
 $db = mysqli_connect($host, $username, $password, $database, $port) or die("Database connection failed.");
