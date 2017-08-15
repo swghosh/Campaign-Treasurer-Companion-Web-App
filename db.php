074ca980e9b222b0195373846432e9e7de1904f4
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-15 19:35:08 +0530

    Added db.php for RDS instance mysql db conn

--- db.php
+++ db.php
@@ -1,1 +1,9 @@
 <?php
+//Set database settings
+$host = getenv("RDS_HOST_NAME");
+$username = getenv("RDS_USERNAME");
+$password = getenv("RDS_PASSWORD");
+$port = getenv("RDS_PORT");
+$database = getenv("RDS_DB_NAME");
+//-----------------------------
+$db = mysqli_connect($host, $username, $password, $database, $port) or die("Database connection failed.");
