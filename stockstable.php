7391cc7bbed23573f25c0f1a78064ea157e4a33a
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-18 22:46:36 +0530

    Changes to stockstable.php to include currency symbols in table and fix negative symbols in difference and percentage by use of colored triangles

--- stockstable.php
+++ stockstable.php
@@ -8,9 +8,9 @@
     <?php
         include_once('db.php');
 
-        $sql_cryptocurrencies = "SELECT name, current FROM cryptocurrencies;";
-        $sql_commodities = "SELECT name, current FROM commodities;";
-        $sql_stocks = "SELECT name, sector, current, difference, percentage FROM stocks;";
+        $sql_cryptocurrencies = "SELECT name, current FROM cryptocurrencies ORDER BY name;";
+        $sql_commodities = "SELECT name, current FROM commodities ORDER BY name;";
+        $sql_stocks = "SELECT name, sector, current, difference, percentage FROM stocks ORDER BY name;";
 
     ?>
             <tr class="sector"><td colspan="4" class="sector">Cryptocurrencies</td></tr> 
@@ -19,7 +19,7 @@
         while($ar = mysqli_fetch_array($res)) {
             $name = $ar['name'];
             $current = $ar['current'];
-            $str = '<tr class="stock"><td class="name">'.$name.'</td><td class="current" colspan="3">'.$current.'</td></tr>';
+            $str = '<tr class="stock"><td class="name">'.$name.'</td><td class="current" colspan="3">$'.$current.'</td></tr>';
             echo $str."\n";
         }
     ?>
@@ -30,7 +30,7 @@
         while($ar = mysqli_fetch_array($res)) {
             $name = $ar['name'];
             $current = $ar['current'];
-            $str = '<tr class="stock"><td class="name">'.$name.'</td><td class="current" colspan="3">'.$current.'</td></tr>';
+            $str = '<tr class="stock"><td class="name">'.$name.'</td><td class="current" colspan="3">$'.$current.'</td></tr>';
             echo $str."\n";
         }
     ?>
@@ -44,7 +44,18 @@
             $current = $ar['current'];
             $difference = $ar['difference'];
             $percentage = $ar['percentage'];
-            $str = '<tr class="stock"><td class="name">'.$name.'<br><small>'.$sector.'</small></td><td class="current">'.$current.'</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>'.$difference.'</td><td class="percentage high">'.$percentage.'</td></tr>';
+
+            $str = '';
+            if($difference < 0) {
+                $difference = -$difference;
+                $percentage = -$percentage;
+
+                $str = '<tr class="stock"><td class="name">'.$name.'<br><small>'.$sector.'</small></td><td class="current">$'.$current.'</td><td class="difference"><span class="not_inverted triangle">&#9650;</span>$'.$difference.'</td><td class="percentage high">'.$percentage.'%</td></tr>';
+            }
+            else {
+                $str = '<tr class="stock"><td class="name">'.$name.'<br><small>'.$sector.'</small></td><td class="current">$'.$current.'</td><td class="difference"><span class="inverted triangle">&#9660;</span>$'.$difference.'</td><td class="percentage low">'.$percentage.'%</td></tr>';
+            }
+
             echo $str."\n";
         }
     ?>
