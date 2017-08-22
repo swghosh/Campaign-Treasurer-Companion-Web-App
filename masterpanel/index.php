4e3299fc5f8c6307ae2b20dca0d0a2ae5ce65c94
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-23 01:17:12 +0530

    Links in masterpanel updated

--- masterpanel/index.php
+++ masterpanel/index.php
@@ -39,7 +39,7 @@
                 </form>
                 <br><br>
             </td></tr>
-            <tr class="stock" onclick="document.location = 'rollbacknews';"><td class="name"><a href="rollbacknews" class="link">Rollback News Item <span class="arrow ext">⎋</span></a></td></tr>
+            <tr class="stock" onclick="document.location = 'rollbacknews.php';"><td class="name"><a href="rollbacknews.php" class="link">Rollback News Item <span class="arrow ext">⎋</span></a></td></tr>
 
             <tr class="sector"><td colspan="4" class="sector">Cryptocurrencies, Comodities, Securities</td></tr>
             <tr class="stock" id="addstock"><td class="name">Add New Cryptocurrency / Commodity / Security <span id="addstock" class="arrow">⌵</span></td></tr>
@@ -91,7 +91,7 @@
                 </form>
                 <br><br>
             </td></tr>
-            <tr class="stock" onclick="document.location = 'rollbackprice';"><td class="name"><a href="rollbackprice" class="link">Rollback Price Update for Cryptocurrency / Commodity / Security <span class="arrow ext">⎋</span></a></td></tr>
+            <tr class="stock" onclick="document.location = 'rollbackprice.php';"><td class="name"><a href="rollbackprice.php" class="link">Rollback Price Update for Cryptocurrency / Commodity / Security <span class="arrow ext">⎋</span></a></td></tr>
 
             <tr class="sector"><td colspan="4" class="sector">Larger Display</td></tr>
             <tr class="stock" onclick="document.location = 'largescreenview.php';"><td class="name"><a href="largescreenview.php" class="link">Show Market Overview <span class="arrow ext">⎋</span></a></td></tr>
@@ -105,7 +105,7 @@
                 </form>
                 <br><br>
             </td></tr>
-            <tr class="stock" onclick="document.location = 'activebuyers';"><td class="name"><a href="activebuyers" class="link">Show Active Buyers <span class="arrow ext">⎋</span></a></td></tr>
+            <tr class="stock" onclick="document.location = 'activebuyers.php';"><td class="name"><a href="activebuyers.php" class="link">Show Active Buyers <span class="arrow ext">⎋</span></a></td></tr>
             <tr class="stock" id="grantfunds"><td class="name">Grant Funds <span id="grantfunds" class="arrow">⌵</span></td></tr>
             <tr class="news" id="grantfunds"><td class="time"></td><td class="news">
                 <form method="POST" action="grantfunds.php">
