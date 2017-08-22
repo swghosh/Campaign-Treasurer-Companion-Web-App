4b8b0b247f3033ba33e2aad2738edd8317132d78
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-22 23:17:01 +0530

    Fixed view switching of sector input upon selection of cryptocurrency/commodity/security in addstock part

--- masterpanel/inpanel.js
+++ masterpanel/inpanel.js
@@ -4,6 +4,7 @@ var pcloseInput = document.querySelector('input[name=pclose]');
 var lcircuitInput = document.querySelector('input[name=lcircuit]');
 var ucircuitInput = document.querySelector('input[name=ucircuit]');
 var ovalueInput = document.querySelector('input[name=ovalue]');
+var sectorInput = document.querySelector('input[name=sector]');
 
 var pcloseLabel = document.querySelector('label[for=pclose]');
 var lcircuitLabel = document.querySelector('label[for=lcircuit]');
@@ -16,10 +17,12 @@ selectType.onchange = function() {
         pcloseInput.disabled = null;
         lcircuitInput.disabled = null;
         ucircuitInput.disabled = null;
+        sectorInput.disabled = null;
 
         pcloseInput.style.display = null;
         lcircuitInput.style.display = null;
         ucircuitInput.style.display = null;
+        sectorInput.style.display = null;
 
         pcloseLabel.style.display = null;
         lcircuitLabel.style.display = null;
@@ -29,10 +32,12 @@ selectType.onchange = function() {
         pcloseInput.disabled = 'yes';
         lcircuitInput.disabled = null;
         ucircuitInput.disabled = null;
+        sectorInput.disabled = 'yes';
 
         pcloseInput.style.display = 'none';
         lcircuitInput.style.display = null;
         ucircuitInput.style.display = null;
+        sectorInput.style.display = 'none';
 
         pcloseLabel.style.display = 'none';
         lcircuitLabel.style.display = null;
@@ -42,10 +47,12 @@ selectType.onchange = function() {
         pcloseInput.disabled = null;
         lcircuitInput.disabled = null;
         ucircuitInput.disabled = null;
+        sectorInput.disabled = null;
 
         pcloseInput.style.display = 'none';
         lcircuitInput.style.display = 'none';
         ucircuitInput.style.display = 'none';
+        sectorInput.style.display = 'none';
 
         pcloseLabel.style.display = 'none';
         lcircuitLabel.style.display = 'none';
