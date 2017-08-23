8364a3644792ec1d51c37fccfb76a8a89bff986e
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-24 01:57:18 +0530

    Added dummy content in trader/index.php

--- trader/index.php
+++ trader/index.php
@@ -1,6 +1,21 @@
 <?php 
     include('../head.php'); 
 ?>
+    <table class="view">
+        <tr class="sector">
+            <td class="user">username: <span class="user">alpha</span> (logged-in)<br>available balance: <span class="user">$0.00</span></td>
+        </tr>
+        
+
+        <tr class="sector"><td colspan="4" class="sector">Transact</td></tr>
+        <tr class="stock"><td class="name">Buy Commodity / Cryptocurrency / Security <span id="about" class="arrow">⌵</span></td></tr>
+        <tr class="stock"><td class="name">Sell Commodity / Cryptocurrency / Security <span id="rules" class="arrow">⌵</span></td></tr>
+
+        <tr class="sector"><td colspan="4" class="sector">History</td></tr>
+        <tr class="stock"><td class="name">Purchased<span id="about" class="arrow">⌵</span></td></tr>
+        <tr class="stock"><td class="name">Order Book<span id="rules" class="arrow">⌵</span></td></tr>
+
+    </table>
 <?php 
     $scripts = array();
     include('../foot.php'); 
