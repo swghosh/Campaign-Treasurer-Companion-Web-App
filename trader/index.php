a53af3a6ad3faf1fa3c624baedb489ea5fa4f8da
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-24 18:54:20 +0530

    Added code in trader/index.php to show balance

--- trader/index.php
+++ trader/index.php
@@ -1,21 +1,35 @@
 <?php 
     include('../head.php'); 
+    include('tradingfunctions.php');
 
     $username = $_SERVER['REMOTE_USER'];
+    $balance = balance($username);
 ?>
     <table class="view">
         <tr class="sector">
-            <td class="user">username: <span class="user"><?php echo $username; ?></span> (logged-in)<br>available balance: <span class="user">$0.00</span></td>
+            <td class="user">username: <span class="user"><?php echo $username; ?></span> (logged-in)<br>available balance: <span class="user">$<?php echo $balance; ?></span></td>
         </tr>
         
 
         <tr class="sector"><td colspan="4" class="sector">Transact</td></tr>
         <tr class="stock"><td class="name">Buy Commodity / Cryptocurrency / Security <span id="about" class="arrow">⌵</span></td></tr>
+        <tr class="news"><td class="time"></td><td class="news">
+            <br><br>
+        </td></tr>
         <tr class="stock"><td class="name">Sell Commodity / Cryptocurrency / Security <span id="rules" class="arrow">⌵</span></td></tr>
+        <tr class="news"><td class="time"></td><td class="news">
+            <br><br>
+        </td></tr>
 
         <tr class="sector"><td colspan="4" class="sector">History</td></tr>
         <tr class="stock"><td class="name">Purchased<span id="about" class="arrow">⌵</span></td></tr>
+        <tr class="news"><td class="time"></td><td class="news">
+            <br><br>
+        </td></tr>
         <tr class="stock"><td class="name">Order Book<span id="rules" class="arrow">⌵</span></td></tr>
+        <tr class="news"><td class="time"></td><td class="news">
+            <br><br>
+        </td></tr>
 
     </table>
 <?php 
