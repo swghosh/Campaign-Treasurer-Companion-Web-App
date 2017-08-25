ffceffc0621f72b70dc9212f2020eec1d3c38558
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-25 12:07:58 +0530

    Added code in userbalances.php to display available balance of all users

---
+++ masterpanel/userbalances.php
@@ -0,0 +1,26 @@
+<?php
+    include('head.php');
+
+    include('../db.php');
+?>
+        <table class="view" id="panel">
+        <tr class="sector"><td colspan="4" class="sector">Navigate</td></tr>
+        <tr class="stock" onclick="document.location = '/masterpanel';"><td class="name"><a href="/masterpanel" class="link">Master Panel<span class="arrow ext">⎋</span></a></td></tr>
+
+        <tr class="sector"><td colspan="4" class="sector">User Balances</td></tr>
+        <?php
+            $sql = "SELECT name, balance FROM users;";
+            $res = mysqli_query($db, $sql);
+
+            while($ar = mysqli_fetch_array($res)) {
+                $name = $ar['name'];
+                $balance = $ar['balance'];
+                $str = '<tr class="stock"><td class="name">'.$name.'</td><td class="current">'.$balance.'</td></tr>'."\n";
+                print($str);
+            }
+        ?>
+        </table>    
+<?php
+    $scripts = array();
+    include('foot.php');
+?>
