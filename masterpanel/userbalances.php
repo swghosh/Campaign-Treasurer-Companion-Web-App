ac0b2da96536e77d83e48e160d18aabad70891ff
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-25 12:11:45 +0530

    Tr colspan adjust in userbalances.php of masterpanel

--- masterpanel/userbalances.php
+++ masterpanel/userbalances.php
@@ -5,7 +5,7 @@
 ?>
         <table class="view" id="panel">
         <tr class="sector"><td colspan="4" class="sector">Navigate</td></tr>
-        <tr class="stock" onclick="document.location = '/masterpanel';"><td class="name"><a href="/masterpanel" class="link">Master Panel<span class="arrow ext">⎋</span></a></td></tr>
+        <tr class="stock" onclick="document.location = '/masterpanel';"><td class="name" colspan="4"><a href="/masterpanel" class="link">Master Panel<span class="arrow ext">⎋</span></a></td></tr>
 
         <tr class="sector"><td colspan="4" class="sector">User Balances</td></tr>
         <?php
