b467bfad0c4f0f949c32d1a28ecbb5e0984d4539
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-25 21:59:32 +0530

    Added code in buy.php, sell.php to allow trading using form submission under trader

--- masterpanel/activebuyers.php
+++ masterpanel/activebuyers.php
@@ -5,7 +5,7 @@
 ?>
         <table class="view" id="panel">
         <tr class="sector"><td colspan="2" class="sector">Navigate</td></tr>
-        <tr class="stock" onclick="document.location = '/masterpanel';"><td class="name"><a href="/masterpanel" class="link">Master Panel<span class="arrow ext">⎋</span></a></td></tr>
+        <tr class="stock" onclick="document.location = '/masterpanel';"><td class="name" colspan="2"><a href="/masterpanel" class="link">Master Panel<span class="arrow ext">⎋</span></a></td></tr>
 
         <tr class="sector"><td colspan="2" class="sector">Active Buyers</td></tr>
         <?php
--- trader/buy.php
+++ trader/buy.php
@@ -1,2 +1,31 @@
 <?php
 $username = $_SERVER['REMOTE_USER'];
+
+include('tradingfunctions.php');
+
+function form_error() {
+    die('An error occured.');
+}
+
+// form validation
+if(isset($_POST['item']) == false || empty($_POST['item']) || isset($_POST['quantity']) == false || empty($_POST['quantity'])) {
+    form_error();
+}
+
+$item = mysqli_real_escape_string($db, htmlspecialchars($_POST['item']));
+$quantity = mysqli_real_escape_string($db, htmlspecialchars($_POST['quantity']));
+
+$purchased = purchased($username);
+$value = 0;
+if($price = price($item)) {
+    $value = $price * $quantity;
+    if($balance < $value) {
+        form_error();
+    }
+}
+
+if(transact_sell($username, $item, $quantity)) {
+    die('Successfully processed the request.');
+}
+
+die('Unsuccessful, while processing the request.');
