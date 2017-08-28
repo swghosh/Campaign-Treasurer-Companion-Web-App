c84e98e8a2925f80598443e1aa284517f35d8574
SwG Ghosh <codecrafts.cf@icloud.com>
2017-08-28 23:56:25 +0530

    Fixes to remove ucircuit, lcircuit

--- masterpanel/addstock.php
+++ masterpanel/addstock.php
@@ -16,7 +16,7 @@ $type = $_POST['type'];
 
 if($type == 'stock') {
     // form validation
-    if(isset($_POST['name']) == false || empty($_POST['name']) || isset($_POST['sector']) == false || empty($_POST['sector']) || isset($_POST['pclose']) == false || empty($_POST['pclose']) || isset($_POST['ovalue']) == false || empty($_POST['ovalue']) || isset($_POST['lcircuit']) == false || empty($_POST['lcircuit']) || isset($_POST['ucircuit']) == false || empty($_POST['ucircuit']) || isset($_POST['description']) == false || empty($_POST['description'])) {
+    if(isset($_POST['name']) == false || empty($_POST['name']) || isset($_POST['sector']) == false || empty($_POST['sector']) || isset($_POST['pclose']) == false || empty($_POST['pclose']) || isset($_POST['ovalue']) == false || empty($_POST['ovalue']) || isset($_POST['description']) == false || empty($_POST['description'])) {
         form_error();
     }
 
@@ -24,8 +24,6 @@ if($type == 'stock') {
     $sector = mysqli_real_escape_string($db, htmlspecialchars($_POST['sector']));
     $pclose = mysqli_real_escape_string($db, htmlspecialchars($_POST['pclose']));
     $ovalue = mysqli_real_escape_string($db, htmlspecialchars($_POST['ovalue']));
-    $lcircuit = mysqli_real_escape_string($db, htmlspecialchars($_POST['lcircuit']));
-    $ucircuit = mysqli_real_escape_string($db, htmlspecialchars($_POST['ucircuit']));
     $description = mysqli_real_escape_string($db, htmlspecialchars($_POST['description']));
 
     $current = $ovalue;
@@ -33,7 +31,7 @@ if($type == 'stock') {
     $percentage = ($difference / $pclose) * 100;
 
     // database query statements
-    $sql = "INSERT INTO stocks (name, sector, current, difference, percentage, pclose, ovalue, lcircuit, ucircuit, description) VALUES ('$name', '$sector', $current, $difference, $percentage, $pclose, $ovalue, $lcircuit, $ucircuit, '$description');";
+    $sql = "INSERT INTO stocks (name, sector, current, difference, percentage, pclose, ovalue, description) VALUES ('$name', '$sector', $current, $difference, $percentage, $pclose, $ovalue, '$description');";
     date_default_timezone_set('Asia/Kolkata');
     $time = date('Y-m-d H:i:s');
     $sql2 = "INSERT INTO updates (name, current, difference, percentage, time) VALUES ('$name', $pclose, 0, 0, '$time'), ('$name', $current, $difference, $percentage, '$time');";
@@ -47,20 +45,18 @@ if($type == 'stock') {
 }
 else if($type == 'commodity') {
     // form validation
-    if(isset($_POST['name']) == false || empty($_POST['name']) || isset($_POST['ovalue']) == false || empty($_POST['ovalue']) || isset($_POST['lcircuit']) == false || empty($_POST['lcircuit']) || isset($_POST['ucircuit']) == false || empty($_POST['ucircuit']) || isset($_POST['description']) == false || empty($_POST['description'])) {
+    if(isset($_POST['name']) == false || empty($_POST['name']) || isset($_POST['ovalue']) == false || empty($_POST['ovalue']) || isset($_POST['description']) == false || empty($_POST['description'])) {
         form_error();
     }
 
     $name = mysqli_real_escape_string($db, htmlspecialchars($_POST['name']));
     $ovalue = mysqli_real_escape_string($db, htmlspecialchars($_POST['ovalue']));
-    $lcircuit = mysqli_real_escape_string($db, htmlspecialchars($_POST['lcircuit']));
-    $ucircuit = mysqli_real_escape_string($db, htmlspecialchars($_POST['ucircuit']));
     $description = mysqli_real_escape_string($db, htmlspecialchars($_POST['description']));
     
     $current = $ovalue;
 
     // database query statements
-    $sql = "INSERT INTO commodities (name, current, ovalue, lcircuit, ucircuit, description) VALUES ('$name', $current, $ovalue, $lcircuit, $ucircuit, '$description');";
+    $sql = "INSERT INTO commodities (name, current, ovalue, description) VALUES ('$name', $current, $ovalue, '$description');";
     date_default_timezone_set('Asia/Kolkata');
     $time = date('Y-m-d H:i:s');
     $sql2 = "INSERT INTO updates (name, current, time) VALUES ('$name', $current, '$time');";
